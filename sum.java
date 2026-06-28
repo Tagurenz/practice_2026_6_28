a6cd759a792e1734bfb67b7c2ed5b2d3fa00c69b
Tagurenz <hiroyuki150514@au.com>
2026-06-29 00:34:59 +0900

    Fix sum method signature and add pull method

--- sum.java
+++ sum.java
@@ -1,5 +1,11 @@
-public static class sum(int a, int b){
+public static int sum(int a, int b){
     int sum;
     sum = a + b;
     return sum;
+}
+
+public static int pull(int a, int b){
+    int pull;
+    pull = a - b;
+    return pull;
 }
